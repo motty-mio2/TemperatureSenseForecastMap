f2a4cb8cbeabe3728b4c13d79995fb565bdde74e
motty <motty.mio2@gmail.com>
2022-09-16 04:13:40 +0900

    contour option

--- app/db2geojson.py
+++ app/db2geojson.py
@@ -2,9 +2,16 @@ import json
 import pathlib
 from datetime import datetime, timedelta, timezone
 from typing import Any
+import matplotlib.pyplot as plt
 
-from .models import point_name
+<<<<<<< main
 from .wbgt_util import wbgt_status
+from .models import point_name
+import cartopy.crs as ccrs
+
+=======
+>>>>>>> contour option
+# from .models import point_name
 
 
 class wbgt_point:
@@ -69,5 +76,55 @@ def data2geojson(file_name: str = "info.geojson", force: bool = False):
                 pass
 
         with open(target, mode="w+") as f:
-
             json.dump(g.__dict__, f)
+
+
+def sample_data(shape=(73, 145)):
+    """Return ``lons``, ``lats`` and ``data`` of some fake data."""
+    nlats, nlons = shape
+    lats = np.linspace(-np.pi / 2, np.pi / 2, nlats)
+    lons = np.linspace(0, 2 * np.pi, nlons)
+    lons, lats = np.meshgrid(lons, lats)
+    wave = 0.75 * (np.sin(2 * lats) ** 8) * np.cos(4 * lons)
+    mean = 0.5 * np.cos(2 * lats) * ((np.sin(2 * lats)) ** 2 + 2)
+
+    lats = np.rad2deg(lats)
+    lons = np.rad2deg(lons)
+    data = wave + mean
+
+    print(data)
+
+    return lons, lats, data
+
+
+def contourf(file_name: str = "info.geojson", hour=1):
+    json_data = json.load(open(pathlib.Path(__file__).parent / "static" / file_name, mode="r"))
+
+    idos = []
+    keidos = []
+    seviraritys = []
+
+    for point_info in json_data["features"]:
+        geo, prop = point_info["geometry"], point_info["properties"]
+
+        if prop["Hour"] == hour:
+            idos.append(geo["coordinates"][1])
+            keidos.append(geo["coordinates"][0])
+            seviraritys.append(prop["sevirarity"])
+
+    import folium
+    from folium import plugins
+    import random
+
+    center = [32, 131]
+    m = folium.Map(location=center, zoom_start=5)
+
+    folium.plugins.HeatMap(
+        data=[(ido, keido, random.randint(0, 100)) for ido, keido, data in zip(idos, keidos, seviraritys)],  # ２次元を渡す
+        radius=15,
+    ).add_to(m)
+    m.save("pcr_positive.html")
+
+
+if __name__ == "__main__":
+    contourf()
