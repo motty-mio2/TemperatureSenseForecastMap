0434f1dbd7b4df48bedff873347e2bdd10f36dab
motty.mio2 <motty.mio2@gmail.com>
2022-12-07 01:05:36 +0900

    update clock settings

--- app/clock.py
+++ app/clock.py
@@ -14,9 +14,7 @@ def test_job():
         ido = point.ido
         keido = point.keido
 
-        print("id:{} 地点名:{}".format(point.id, point.name))
-        # print("ido", ido)
-        # print("keido", keido)
+        print(f"id:{point.id} 地点名:{point.name}")
 
         try:
             wbgts_list, time_list = wbgt_util.location2wbgt(ido, keido, hours=48)
@@ -44,9 +42,12 @@ def sample():
 
 
 def start():
-    db2geojson.data2geojson()
+    try:
+        db2geojson.data2geojson()
+    except KeyError:
+        pass
 
     scheduler = BackgroundScheduler()
     hour, minute = map(int, os.environ.get("CLOCK_TIME", "00:00").split(":"))
-    scheduler.add_job(test_job, "cron", hour=hour, minute=minute)  # 毎日23時59分に実行
+    scheduler.add_job(test_job, "cron", hour=hour, minute=minute, misfire_grace_time=300)  # 毎日23時59分に実行
     scheduler.start()
